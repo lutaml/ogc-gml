1cf8181f7b02e0f51049091490232f313fc138c2
Ronald Tse <ronald.tse@ribose.com>
2024-05-21 00:51:45 +0800

    feat: initial commit

---
+++ lib/ogc/gml.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "gml/version"
+
+module Ogc
+  module Gml
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/ogc/gml_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ogc::Gml do
+  it "has a version number" do
+    expect(Ogc::Gml::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
