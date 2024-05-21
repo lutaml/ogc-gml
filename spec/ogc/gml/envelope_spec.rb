9172f5defcd9ff52fcb7ae5eaf2f0bf3902e234b
Ronald Tse <ronald.tse@ribose.com>
2024-05-21 10:49:56 +0800

    feat: add Envelope tests

---
+++ spec/ogc/gml/envelope_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RSpec.describe Ogc::Gml::Envelope do
+
+  def file_contents(filename)
+    File.read(
+      Pathname.new(__dir__)
+        .join("../../../ets-gml32/src/test/resources/envelopes/#{filename}")
+    ).gsub("\t", "  ")
+  end
+
+  %w(
+    Envelope-httpRef.xml
+    Envelope-invalidCRS.xml
+    Envelope-invalidCorner.xml
+    Envelope-noCRS.xml
+    Envelope-valid.xml
+  ).each do |filename|
+    it "round-trips #{filename}" do
+      input = file_contents(filename)
+      output = Ogc::Gml::Envelope.from_xml(input).to_xml(
+        pretty: true,
+        declaration: true,
+        encoding: "utf-8"
+      )
+
+      expect(output).to be_equivalent_to(input)
+    end
+  end
+end
