bb7f7b4a9d5489a44870eb429fdc628cc85d541e
Ronald Tse <ronald.tse@ribose.com>
2024-05-22 16:25:36 +0800

    feat: add gml:Dictionary tests from https://www.geospatial.jp/iur/codelists/3.1/, fixes #3

--- lib/ogc/gml/dictionary.rb
+++ lib/ogc/gml/dictionary.rb
@@ -3,7 +3,6 @@
 # --- dictionary_type.rb ---
 require "shale"
 
-require_relative "code"
 require_relative "code_with_authority"
 require_relative "dictionary_entry"
 require_relative "indirect_entry"
@@ -13,31 +12,34 @@ require_relative "reference"
 module Ogc
   module Gml
     class Dictionary < Shale::Mapper
-      attribute :id, Shale::Type::Value
+      attribute :id, Shale::Type::String
       attribute :aggregation_type, Shale::Type::String
       attribute :meta_data_property, MetaDataProperty, collection: true
       attribute :description, Shale::Type::String
       attribute :description_reference, Reference
       attribute :identifier, CodeWithAuthority
-      attribute :name, Code, collection: true
+      attribute :name, Shale::Type::String, collection: true
       attribute :remarks, Shale::Type::String
       attribute :dictionary_entry, DictionaryEntry, collection: true
       attribute :indirect_entry, IndirectEntry, collection: true
+      attribute :schema_location, Shale::Type::String
 
       xml do
-        root "DefinitionCollection"
+        root "Dictionary"
         namespace "http://www.opengis.net/gml/3.2", "gml"
 
+        map_attribute "schemaLocation", to: :schema_location, prefix: "xsi", namespace: "http://www.w3.org/2001/XMLSchema-instance"
         map_attribute "id", to: :id, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
-        map_attribute "aggregationType", to: :aggregation_type
-        map_element "metaDataProperty", to: :meta_data_property
-        map_element "description", to: :description
-        map_element "descriptionReference", to: :description_reference
-        map_element "identifier", to: :identifier
-        map_element "name", to: :name
+        map_attribute "aggregationType", to: :aggregation_type, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+
+        map_element "metaDataProperty", to: :meta_data_property, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "description", to: :description, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "descriptionReference", to: :description_reference, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "identifier", to: :identifier, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "name", to: :name, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
         map_element "remarks", to: :remarks
-        map_element "dictionaryEntry", to: :dictionary_entry
-        map_element "indirectEntry", to: :indirect_entry
+        map_element "dictionaryEntry", to: :dictionary_entry, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "indirectEntry", to: :indirect_entry, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
       end
     end
   end
