65305b31d12524aec2b61f6134545dfd6449d5c3
Ronald Tse <ronald.tse@ribose.com>
2024-05-22 16:25:36 +0800

    feat: add gml:Dictionary tests from https://www.geospatial.jp/iur/codelists/3.1/, fixes #3

--- lib/ogc/gml/dictionary_entry.rb
+++ lib/ogc/gml/dictionary_entry.rb
@@ -8,19 +8,25 @@ require_relative "definition"
 module Ogc
   module Gml
     class DictionaryEntry < Shale::Mapper
-      attribute :owns, Shale::Type::Boolean, default: -> { "false" }
+      attribute :id, Shale::Type::String
+      attribute :owns, Shale::Type::Boolean#, default: -> { "false" }
       attribute :nil_reason, Shale::Type::Value
       attribute :remote_schema, Shale::Type::Value
       attribute :definition, Definition
+      attribute :name, Shale::Type::String
+      attribute :description, Shale::Type::String
 
       xml do
-        root "definitionMember"
+        root "dictionaryEntry"
         namespace "http://www.opengis.net/gml/3.2", "gml"
 
+        map_attribute "id", to: :id, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
         map_attribute "owns", to: :owns
         map_attribute "nilReason", to: :nil_reason
         map_attribute "remoteSchema", to: :remote_schema, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
-        map_element "Definition", to: :definition
+        map_element "Definition", to: :definition, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "description", to: :description, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
+        map_element "name", to: :name, prefix: "gml", namespace: "http://www.opengis.net/gml/3.2"
       end
     end
   end
