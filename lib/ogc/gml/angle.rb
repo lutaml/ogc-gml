0a2837597eda8d39392c0952c10d8cf3846b0015
Ronald Tse <ronald.tse@ribose.com>
2024-08-29 20:34:14 +0800

    fix: unit of measure subclasses

--- lib/ogc/gml/angle.rb
+++ lib/ogc/gml/angle.rb
@@ -3,16 +3,18 @@
 require "lutaml/model"
 
 require_relative "measure"
+require_relative "unit_of_measure"
 
 module Ogc
   module Gml
-    class Angle < Lutaml::Model::Serializable
+    class Angle < UnitOfMeasure
       attribute :content, Measure
 
       xml do
         root "greenwichLongitude"
         namespace "http://www.opengis.net/gml/3.2", "gml"
 
+        map_attribute "uom", to: :uom
         map_content to: :content
       end
     end
--- lib/ogc/gml/length.rb
+++ lib/ogc/gml/length.rb
@@ -3,16 +3,18 @@
 require "lutaml/model"
 
 require_relative "measure"
+require_relative "unit_of_measure"
 
 module Ogc
   module Gml
-    class Length < Lutaml::Model::Serializable
+    class Length < UnitOfMeasure
       attribute :content, Measure
 
       xml do
         root "LengthType"
         namespace "http://www.opengis.net/gml/3.2", "gml"
 
+        map_attribute "uom", to: :uom
         map_content to: :content
       end
     end
