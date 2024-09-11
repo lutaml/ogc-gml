d46e4e6aaec77d0fb7d7031df4bcb57bdad3aee1
Ronald Tse <ronald.tse@ribose.com>
2024-09-11 11:57:20 +0800

    chore: rubocop

--- spec/ogc/gml/composite_curve_spec.rb
+++ spec/ogc/gml/composite_curve_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Ogc::Gml::CompositeCurve do
     output = Ogc::Gml::CompositeCurve.from_xml(input).to_xml(
       pretty: true,
       declaration: true,
-      encoding: "utf-8",
+      encoding: "utf-8"
     )
 
     expect(output).to be_equivalent_to(input)
