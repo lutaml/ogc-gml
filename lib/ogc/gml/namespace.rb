eb61fd26c8391aec5c7e69884f10cf6bdfecdf71
Ronald Tse <ronald.tse@ribose.com>
2025-11-25 23:13:43 +0800

    chore: rubocop

--- lib/ogc/gml/namespace.rb
+++ lib/ogc/gml/namespace.rb
@@ -1,10 +1,12 @@
-require 'lutaml/model/xml_namespace'
+# frozen_string_literal: true
+
+require "lutaml/model/xml_namespace"
 
 module Ogc
   module Gml
     class Namespace < Lutaml::Model::XmlNamespace
-      uri 'http://www.opengis.net/gml/3.2'
-      prefix_default 'gml'
+      uri "http://www.opengis.net/gml/3.2"
+      prefix_default "gml"
     end
   end
 end
