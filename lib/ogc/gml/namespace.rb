3305bb7c4ff391239660ca9917d5ca2390bac572
Ronald Tse <ronald.tse@ribose.com>
2026-03-20 07:11:06 +0800

    fix(xml): enable element_form_default and attribute_form_default :qualified
    GML 3.2 schema uses elementFormDefault="qualified" and
    attributeFormDefault="qualified". These settings ensure:
    - Child elements inherit the GML namespace prefix (gml:)
    - Attributes in the GML namespace are namespace-qualified (gml:id)

--- lib/ogc/gml/namespace.rb
+++ lib/ogc/gml/namespace.rb
@@ -7,8 +7,8 @@ module Ogc
     class Namespace < Lutaml::Xml::Namespace
       uri "http://www.opengis.net/gml/3.2"
       prefix_default "gml"
-      # element_form_default :qualified
-      # attribute_form_default :qualified
+      element_form_default :qualified
+      attribute_form_default :qualified
     end
   end
 end
